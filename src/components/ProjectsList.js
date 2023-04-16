3810c00dfdf2c366d52728a5340c39e8a303b0a9
Klair8 <clsacuto@gmail.com>
2023-04-16 23:39:05 +0300

    cards done

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,41 +1,95 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card';
+import * as React from 'react';
+import SpaceInvader from '../assets/SpaceInvader.png';
+import MyBookList from '../assets/MyBookList.png'
+import NLPStoryTelling from '../assets/NLPStoryTelling.png'
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+import CardMedia from '@mui/material/CardMedia';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+
+
+
 
 const Projects =()=>{
-          return (
-            <div className='projectList'>
-            <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Space-Invader</Card.Title>
-                <Card.Text>
-                Space Invader Game, created essentialy with Javascrip .
-                </Card.Text>
-                <a href="https://klair8.github.io/HACKATHON-1" class="btn"><i class="fab fa-github"></i> See Full Project</a>
-              </Card.Body>
-            </Card>
-             <Card>
-             <Card.Img variant="top" src="holder.js/100px180" />
-             <Card.Body>
-               <Card.Title>Book Repertory</Card.Title>
-               <Card.Text>
-                 Book Repertory, created with Node.js and REST API technic, with the use of the Google Book Api
-               </Card.Text>
-               <a href="#" class="btn"><i class="fab fa-github"></i> See Full Project</a>
-             </Card.Body>
-           </Card>
-            <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>NLP Projects</Card.Title>
-              <Card.Text>
-                Learning process for Kids using Node.Js, React and Redux
-              </Card.Text>
-              <a href="#" class="btn"><i class="fab fa-github"></i> See Full Project</a>
-            </Card.Body>
-          </Card>
-          </div>
-          );
-        }
+  return (
+    <div className='cards'>
+     <Card sx={{ maxWidth: 345 , border:"2px solid black", font_family:'Quicksand' }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="280"
+          image={SpaceInvader}
+          alt="Space Invaders Games"
+        />
+        <CardContent>
+          <br></br>
+          <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black" >
+            Space Invaders
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Old School Space Invaders Game only with Javascript.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+      <Button size="small" href="https://klair8.github.io/HACKATHON-1">
+          See More
+        </Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ maxWidth: 345 , border:"2px solid black" }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="280"
+          image={MyBookList}
+          alt="Book Repertory"
+        />
+        <CardContent>
+        <br></br>
+          <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black" >
+          Book Repertory
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Book Repertory, created with Node.js and REST API technic.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+      <Button size="small" href="https://my-booklist.onrender.com">
+          See More
+        </Button>
+      </CardActions>
+    </Card>
+    <Card sx={{ maxWidth: 345 , border:"2px solid black"}}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="280"
+          image={NLPStoryTelling}
+          alt="Story Telling for kids"
+        />
+        <CardContent>
+        <br></br>
+          <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black" >
+          NLP Project
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Learning Process for Kids using Node.Js and React.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+      <Button size="small" href="https://dream-learning.vercel.app">
+          See More
+        </Button>
+      </CardActions>
+    </Card>
+    </div>
+  )
+}
+
 
 export default Projects
