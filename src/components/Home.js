f5bf24d4b0f85bfee90e1b5139b4b8d83c234833
Klair8 <clsacuto@gmail.com>
2023-05-22 14:07:07 +0300

    responsive update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,33 @@
-import React from 'react'
-import Photo2 from '../assets/Photo2.png'
-import { FaGithub,FaLinkedinIn} from 'react-icons/fa';
+import React from 'react';
+import Photo2 from '../assets/Photo2.png';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
-
-const Home =()=>{
-    return(
-        <div className='my-component'>
-        <div className='homePage' >
-        <div className='txtHomepage'>
-            <h1 style={{fontSize:70, fontFamily:'Quicksand-SemiBold'}}>Hello!</h1>
-            <h3 style={{fontSize:40, fontFamily:'Quicksand-Medium'}}>I am Claire Sacuto</h3>
-            <h4 style={{fontSize:30}}> Full Stack Developer</h4>
-            <br></br>
-            <br></br>
-            <div className='links'>
-                <a href="https://github.com/Klair8">
-                    <FaGithub style={{fontSize:55, paddingRight:'30px'}}/>  
-                </a>
-                <a href="https://www.linkedin.com/in/claire-sacuto/">
-                    <FaLinkedinIn style={{fontSize:55}}/>
-                </a>
-            </div>
+const Home = () => {
+  return (
+    <div className="my-component">
+      <div className="homePage">
+        <div className="txtHomepage">
+           
+          <h1 style={{ marginTop:50,fontSize: 70, fontFamily: 'Quicksand-SemiBold' }}>Hello!</h1>
+          <h3 style={{ fontSize: 40, fontFamily: 'Quicksand-Medium' }}>I am Claire Sacuto</h3>
+          <h4 style={{ fontSize: 30 }}>Full Stack Developer</h4>
+          <br />
+          <br />
+          <div className="links">
+            <a href="https://github.com/Klair8">
+              <FaGithub style={{ fontSize: 55, paddingRight: '30px' }} />
+            </a>
+            <a href="https://www.linkedin.com/in/claire-sacuto/">
+              <FaLinkedinIn style={{ fontSize: 55 }} />
+            </a>
+          </div>
         </div>
-         <div className='imgHomepage'>
-         <img src={Photo2}
-              style={{width: 450, height: 450, borderRadius: 450/ 2}} 
-              />
-          </div>
-          </div>
-          </div>
-    )
-}
+        <div className="imgHomepage">
+          <img src={Photo2} style={{ width: 350, height: 350, borderRadius: 350 / 2 }} alt="Profile" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,103 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import Button from '@mui/material/Button'
-import Stack from '@mui/material/Stack'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import C from '../assets/C.png';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 
-const Header =()=>{
-  
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    return(
-      
-      <Stack spacing={2} direction="row" >
-        <Button sx= {{ color: 'inherit' }} component = {Link} to='/'>C/S</Button>
-        {/* color: '#fff'  */}
-      <Button variant="outlined" sx= {{ color: 'inherit', borderRadius: 28 , borderColor: 'black' }} component = {Link} to='/'>Home</Button>
-      <Button variant="outlined" sx= {{ color: 'inherit', borderRadius: 28 , borderColor: 'black'}} component = {Link} to='/About'>About</Button>
-      <Button variant="outlined" sx= {{ color: 'inherit', borderRadius: 28 , borderColor: 'black' }} component = {Link} to='/Projects'>Projects</Button>
-      <Button variant="outlined" sx= {{ color: 'inherit', borderRadius: 28 ,borderColor: 'black' }} component = {Link} to='/Contact'>Contact</Button>
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <Stack
+      spacing={4}
+      direction="row"
+      sx={{
+        marginLeft: '20vh',
+        '@media (max-width: 600px)': {
+          marginLeft: 0,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+      }}
+    >
+      <Link to="/">
+        <img src={C} alt="Logo" style={{ width: '50px', height: '50px' }} />
+      </Link>
+      <Stack
+        direction="row"
+        alignItems="center"
+        sx={{
+          '@media (max-width: 600px)': {
+            display: isMenuOpen ? 'flex' : 'none',
+            flexDirection: 'column',
+            alignItems: 'center',
+          },
+        }}
+      >
+        <Button
+          variant="outlined"
+          sx={{ color: 'inherit', borderRadius: 28, borderColor: 'black', marginRight: '8px' }}
+          component={Link}
+          to="/"
+        >
+          Home
+        </Button>
+        <Button
+          variant="outlined"
+          sx={{ color: 'inherit', borderRadius: 28, borderColor: 'black', marginRight: '8px' }}
+          component={Link}
+          to="/About"
+        >
+          About
+        </Button>
+        <Button
+          variant="outlined"
+          sx={{ color: 'inherit', borderRadius: 28, borderColor: 'black', marginRight: '8px' }}
+          component={Link}
+          to="/Projects"
+        >
+          Projects
+        </Button>
+        <Button
+          variant="outlined"
+          sx={{ color: 'inherit', borderRadius: 28, borderColor: 'black', marginRight: '8px' }}
+          component={Link}
+          to="/Contact"
+        >
+          Contact
+        </Button>
+        </Stack>
+        <Button
+          onClick={() => {
+            window.open(
+              'https://drive.google.com/file/d/1nijyh2BgXX3GoNwcmM85m3OpRknCjSib/view?usp=sharing'
+            );
+          }}
+          sx={{ color: 'inherit' }}
+          className="resumebtn"
+        >
+          <span>Resume</span>
+        </Button>
       <Button
-            onClick={() => {
-              window.open("https://drive.google.com/file/d/19BQfxKH2faDkcNChnz9ULXbCL8zUOsg0/view?usp=sharing");
-            }}
-            sx= {{ color: 'inherit' }} className='resumebtn'><span>Resume</span>
-          </Button>
-      </Stack>
-          );
-        }
-      
+        onClick={handleMenuToggle}
+        sx={{
+          display: { xs: 'block', md: 'none' },
+          color: 'inherit',
+          borderRadius: 28,
+          borderColor: 'black',
+        }}
+      >
+         {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
+      </Button>
+    </Stack>
+  );
+};
 
-export default Header
+export default Header;
