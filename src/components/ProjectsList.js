aa069020fb8634e852ebe0b0cc27208a221db030
Klair8 <clsacuto@gmail.com>
2023-05-22 14:31:43 +0300

    responsive project page

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -7,124 +7,137 @@ import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
 import CardMedia from '@mui/material/CardMedia';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Grid, Button, CardActionArea, CardActions } from '@mui/material';
 
 
-const Projects =()=>{
+
+const Projects = () => {
   return (
-    <div className='cardsprojects'>
-    <Card className= 'card' sx={{ maxWidth: 345 , border:"2px solid black", margin:"10px"}}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="280"
-          image={NLP}
-          alt="Story Telling for kids"
-        />
-        <CardContent>
-        <br></br>
-          <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black" >
-          NLP Project
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          Learning Process for Kids. 
-          Full Stack process using Node.Js and React.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      <Button variant="outlined" sx= {{ borderRadius: 28 }} size="large" href="https://dream-learning.vercel.app">
-          See More
-        </Button>
-        <Button variant="outlined" sx= {{ borderRadius: 28 }} size="large" href="https://github.com/Klair8/Final-Project-Client">
-          Github Code
-        </Button>
-      </CardActions>
-    </Card>
-    <Card className= 'card' sx={{ maxWidth: 345 , border:"2px solid black", margin:"10px"}}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="280"
-          image={MoviesApp}
-          alt="Movies Search App"
-        />
-        <CardContent>
-        <br></br>
-          <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black" >
-          Movies & Series App
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          Search App for Movies and Series. Redux and React.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      <Button variant="outlined" sx= {{ borderRadius: 28 }} size="large" href="https://movies-app-klair8.vercel.app">
-          See More
-        </Button>
-        <Button variant="outlined" sx= {{ borderRadius: 28 }} size="large" href="https://github.com/Klair8/Movies-Series-App">
-          Github Code
-        </Button>
-      </CardActions>
-    </Card>
-    <Card className= 'card' sx={{ maxWidth: 345 , border:"2px solid black", margin:"10px"}}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="280"
-          image={MyBookList}
-          alt="Book Repertory"
-        />
-        <CardContent>
-        <br></br>
-          <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black" >
-          Book Repertory
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-          Book Repertory. Node.js and REST API technic.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      <Button variant="outlined" sx= {{ borderRadius: 28 }} size="large" href="https://my-booklist.onrender.com">
-          See More
-        </Button>
-        <Button variant="outlined" sx= {{ borderRadius: 28 }} size="large" href="https://github.com/Klair8/HACKATHON">
-          Github Code
-        </Button>
-      </CardActions>
-    </Card>
-    <Card className= 'card' sx={{ maxWidth: 345 , border:"2px solid black", margin:"10px"}}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="280"
-          image={SpaceInvader}
-          alt="Space Invaders Games"
-        />
-        <CardContent>
-          <br></br>
-          <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black" >
-            Space Invaders
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Old School Space Invaders Game . Only Javascript.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      <Button variant="outlined" sx= {{ borderRadius: 28 }} size="large" href="https://klair8.github.io/HACKATHON-1">
-          See More
-        </Button>
-        <Button variant="outlined" sx= {{ borderRadius: 28 }} size="large" href="https://github.com/Klair8/HACKATHON-1">
-          Github Code
-        </Button>
-      </CardActions>
-    </Card>
+    <div className="cardsprojects">
+      <Grid container spacing={4} marginLeft={'10vh'} marginRight={'10vh'}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card sx={{ border: "2px solid black", margin: "10px" }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="280"
+                image={NLP}
+                alt="Story Telling for kids"
+              />
+              <CardContent>
+                <br />
+                <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black">
+                  NLP Project
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Learning Process for Kids.
+                  Full Stack process using Node.Js and React.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button variant="outlined" sx={{ borderRadius: 28 }} size="large" href="https://dream-learning.vercel.app">
+                See More
+              </Button>
+              <Button variant="outlined" sx={{ borderRadius: 28 }} size="large" href="https://github.com/Klair8/Final-Project-Server">
+                Github - Server Side -
+              </Button>
+              <Button variant="outlined" sx={{ borderRadius: 28 }} size="large" href="https://github.com/Klair8/Final-Project-Client">
+                Github - Client Side -
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card sx={{ border: "2px solid black", margin: "10px" }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="280"
+                image={MoviesApp}
+                alt="Movies Search App"
+              />
+              <CardContent>
+                <br />
+                <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black">
+                  Movies & Series App
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Search App for Movies and Series. Redux and React.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button variant="outlined" sx={{ borderRadius: 28 }} size="large" href="https://movies-app-klair8.vercel.app">
+                See More
+              </Button>
+              <Button variant="outlined" sx={{ borderRadius: 28 }} size="large" href="https://github.com/Klair8/Movies-Series-App">
+                Github Code
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card sx={{ border: "2px solid black", margin: "10px" }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="280"
+                image={MyBookList}
+                alt="Book Repertory"
+              />
+              <CardContent>
+                <br />
+                <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black">
+                  Book Repertory
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Book Repertory. Node.js and REST API technic.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button variant="outlined" sx={{ borderRadius: 28 }} size="large" href="https://my-booklist.onrender.com">
+                See More
+              </Button>
+              <Button variant="outlined" sx={{ borderRadius: 28 }} size="large" href="https://github.com/Klair8/HACKATHON">
+                Github Code
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card sx={{ border: "2px solid black", margin: "10px" }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="280"
+                image={SpaceInvader}
+                alt="Space Invaders Games"
+              />
+              <CardContent>
+                <br />
+                <Typography gutterBottom variant="h5" component="div" level="h1" fontSize="lg" textColor="black">
+                  Space Invaders
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Old School Space Invaders Game. Only Javascript.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button variant="outlined" sx={{ borderRadius: 28 }} size="large" href="https://klair8.github.io/HACKATHON-1">
+                See More
+              </Button>
+              <Button variant="outlined" sx={{ borderRadius: 28 }} size="large" href="https://github.com/Klair8/HACKATHON-1">
+                Github Code
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
-  )
+  );
 }
 
-
-export default Projects
+export default Projects;
